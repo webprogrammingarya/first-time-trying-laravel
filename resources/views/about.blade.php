9e679211db801c5a0507fbe174cd5f1fed3a9664
Arya Dimas <webprogrammingarya>
2023-12-03 15:53:26 +0700

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,14 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DPLab Blog | About</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <h1>Halaman About</h1>
+    <h3><?= $name; ?></h3>
+    <h3><?= $email; ?></h3>
+    <img src="img/<?= $image; ?>" alt="<?= $name; ?>" width="200">
+</body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DPLab Blog | Home</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <h1>Halaman Home</h1>
+
+    <script src="js/script.js"></script>
+</body>
+</html>
